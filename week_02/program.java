a4a0ceb3f82755edee4c589b0e536a5ca0b66d80
Adamjatim <adamrahmatilahi@gmail.com>
2022-03-31 11:17:18 +0700

    Update 31/03/2022

---
+++ week_02/program.java
@@ -0,0 +1,72 @@
+/*
+ *  Programmer : Adam Rahmat Ilahi
+ *  Thursday, 31 March 2022
+ * 
+ * Week_02 ALPRO 2 - Elemen Java
+ */
+
+public class program {
+    public static void main(){
+        // Tipe Data
+
+        int Angka = 5;
+        float angka = 6.4;
+        char Huruf = 'A';
+        String Kalimat = "Adam Rahmat ILahi";
+
+        System.out.println("Angka bernilai   : "+Angka);
+        System.out.println("angka bernilai   : "+angka);
+        System.out.println("Huruf bernilai   : "+Huruf);
+        System.out.println("Kalimat bernilai : "+Kalimat);
+
+        System.out.println("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
+        // Aritmatik Operator
+
+        int a, b, c;
+        a = 7;
+        b = 4;
+        c = a + b;
+        System.out.println(a+" + "+b+" = "+c);
+
+        a = 8;
+        b = 2;
+        c = a - b;
+        System.out.println(a+" - "+b+" = "+c);
+
+        a = 3;
+        b = 3;
+        c = a * b;
+        System.out.println(a+" x "+b+" = "+c);
+
+        a = 9;
+        b = 3;
+        c = a / b;
+        System.out.println(a+" / "+b+" = "+c);
+
+        c = a % b;
+        System.out.println(a+" % "+b+" = "+c);
+
+        System.out.println("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
+        // Unary Operator
+
+        a = 6;
+        b = ++a;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+
+        a = 3;
+        b = a++;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+
+        a = 20;
+        b = --a;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+
+        a = 15;
+        b = a--;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+    }
+}
---
+++ week_04/Program_soal_FIX.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Program_soal_FIX{
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Pilihan : ");
+            System.out.println("A. TVRI");
+            System.out.println("B. RCTI");
+            System.out.println("C. SCTV");
+            System.out.println("D. ANTV");
+            System.out.println("E. TRANSTV");
+            System.out.println("F. METROTV");
+            System.out.print("Masukkan Pilihan : ");
+            
+            char pilihan = input.next().charAt(0);
+            System.out.println("======================");
+
+            switch(pilihan){
+                case 'A' :
+                    System.out.println("Masukkan Pilihan : TVRI");
+                    break;
+                case 'B' :
+                    System.out.println("Masukkan Pilihan : RCTI");
+                    break;
+                case 'C' :
+                    System.out.println("Masukkan Pilihan :SCTV");
+                    break;
+                case 'D' :
+                    System.out.println("Masukkan Pilihan :ANTV");
+                    break;
+                case 'E' :
+                    System.out.println("Masukkan Pilihan :TRANSTV");
+                    break;
+                case 'F' :
+                    System.out.println("Masukkan Pilihan :METROTV");
+                    break;
+                default :
+                    System.out.print("Salah Pilih ! ");
+            }
+        }   
+    }
+}
