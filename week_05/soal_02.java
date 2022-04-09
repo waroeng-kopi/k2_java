4544a6482647e19905559a625fe43b98eb6839ad
Adamjatim <adamrahmatilahi@gmail.com>
2022-04-10 02:18:55 +0700

    Update 10/04/2022

--- week_05/soal_02.java
+++ week_05/soal_02.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class soal_02 {
     public static void main(String[] args) {
         try(Scanner Input = new Scanner(System.in)){
-            int var_01, var_02;
+            int var_01, var_02,pengalih;
 
             System.out.print("PROGRAM PENAMPIL BILANGAN NON PRIMA\n");
             System.out.print("Input bilangan Awal  : ");
@@ -29,8 +29,25 @@ public class soal_02 {
             }
 
             else{
-                for(int a = 2; a <= )
+                System.out.println("Bilangan Non Prima : ");
+                for(int awal = var_01; awal <= var_02; awal++){     // untuk membuat loop dari var_01 -> var_02
+                    // untuk proses pengujian
+                    pengalih = 0;
+
+                    for(int uji = 1; uji <= awal; uji++){            // untuk membuat loop yang menguji suatu bilangan dibagi mulai dari angka 1 sampai dirinya sendiri
+                        // menambah nilai 1 pada variabel pengalih
+                        if(awal % uji == 0){
+                            pengalih++;
+                        }
+                    }
+
+                    // berdasarkan rumus diatas jika var_01 % 1 == 0 dan var_01 % var_01 == 0
+                    if(pengalih != 2){
+                        System.out.print(awal+" ");
+                    }
+                }
             }
         }
+        System.out.print("\n");
     }
 }
