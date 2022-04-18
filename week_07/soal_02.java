c4d18a2951c9957cf25d267d3a7a0bbffd444e89
Adamjatim <adamrahmatilahi@gmail.com>
2022-04-18 10:26:28 +0700

    Update 18/04/2022

---
+++ week_07/soal_02.java
@@ -0,0 +1,47 @@
+// Programmer : Adam Rahmat Ilahi
+// Monday, 18 April 2022
+// 
+// Soal :
+//          Mencetak tampilan sesuai dengan inputan
+//      Misal : Input = 4
+//              Output : 1  8  9  4
+//          
+//              Input = 5
+//              Output : 1  16  27  16  5
+//
+
+package week_07;
+
+import java.util.Scanner;
+
+import java.math.*;
+
+public class soal_02 {
+
+    static int Pangkat(int base, int exponent){
+        int hasil = 1; // nllai awal
+
+        for(int a = 1; a <= exponent; a++){
+            hasil = hasil * base; // perkalian pangkat
+        }
+        return hasil;
+    }
+
+    public static void main(String[] args) {
+        try(Scanner Input = new Scanner(System.in)){
+            int input;
+
+            System.out.print("Program Pencetak Nilai Pangkat berurutan terbalik\n");
+            System.out.print("Input nilai : ");
+            input = Input.nextInt();
+
+            // proses
+            int b = input;
+
+            for(int a = 1; a <= input; a++){          
+                System.out.print(Pangkat(a, b) + " ");
+                b--;
+            }
+        }
+    }
+}
